51feace977e12cdd0748f0bfa33d6ae48f787319
Naga Chaitanya Konada <nkonada@deloitte.com>
2018-10-24 23:21:17 +0530

    React integration complete

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './styles.scss';
-console.log('Lets get started...');
+
+import HelloWorld from './components/HelloWorld';
+
+ReactDOM.render(
+    <HelloWorld message="Lets get started..."/>,
+    document.getElementById('root')
+);
