ec9c664f9b19a3f59bee468fc3906e372b5e7c0f
Naga Chaitanya Konada <nkonada@deloitte.com>
2018-10-24 22:53:59 +0530

    dev mode enabled.

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,11 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    mode: 'development'
 };
 
